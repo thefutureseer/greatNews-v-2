f23ee834c3c97d80ba5b2507894d077e480ee32b
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2020-08-12 13:54:26 -0700

    add react

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Button to click and run scrape */}
+      <a href="http://localhost:8000">Scrape now</a>
+    </div>
+  );
+}
+
+export default App;
